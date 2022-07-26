b175d8feb203cf4516b3c8f4b9ac2d83e62734ae
Allison H <eherazo@lsv-tech.com>
2022-07-26 10:29:37 -0500

    terminacion de proyecto

--- modelado.py
+++ modelado.py
@@ -1,3 +1,4 @@
+# library for python/postgres communication
 import psycopg2
 
 # Connect to an existing database
@@ -8,29 +9,58 @@ conn.autocommit = True
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
-# Execute a command: this create table fecha_accidente
-'''cur.execute(
-    "CREATE TABLE IF NOT EXISTS volumetria_accidentalidad_dia.fecha_accidente (id serial PRIMARY KEY UNIQUE, fecha date, dia_semana_acc character varying(50), hora_acc integer);")
-'''
+# ------------------------------ table creation block -----------------------------------------------------
 
-# Execute a command: this create table condicion_meteor
-'''cur.execute(
-    "CREATE TABLE IF NOT EXISTS volumetria_accidentalidad_dia.condicion_meteor (id serial PRIMARY KEY UNIQUE, condicion character varying(20));")
-'''
+# Execute a command: this create table calendar
+'''cur.execute("create table volumetria_accidentalidad_dia.calendar(id_calendar SERIAL primary key unique,date_actual date,day_name varchar (20),day_of_week int,month_name varchar(20),year int);")'''
 
-# Execute a command: this create table terreno
-'''cur.execute(
-    "CREATE TABLE IF NOT EXISTS volumetria_accidentalidad_dia.terreno (id serial PRIMARY KEY UNIQUE, terreno character varying(50), estado_superficie character varying(50));")
-'''
+# Execute a command: this create table territorio
+'''cur.execute("create table volumetria_accidentalidad_dia.territorio(id_territorio SERIAL primary key unique, territorial varchar(50));")'''
 
-# Execute a command: this create table accidentalidad_diaria
+# Execute a command: this create table condicion_meteorologica
+'''cur.execute("create table volumetria_accidentalidad_dia.condicion_meteorologica(id_cond_meteor SERIAL primary key unique,condic_meteor varchar(20));")'''
+
+
+# Execute a command: this create table estado_carretera
+'''cur.execute("create table volumetria_accidentalidad_dia.estado_carretera(id_estado_carretera SERIAL primary key unique,id_cond_meteor int REFERENCES volumetria_accidentalidad_dia.condicion_meteorologica(id_cond_meteor),estado_super varchar(50),terreno varchar(50));")'''
+
+# Execute a command: this create table ubicacion
+'''cur.execute("create table volumetria_accidentalidad_dia.ubicacion(id_ubicacion SERIAL primary key unique,id_terrotirio int REFERENCES volumetria_accidentalidad_dia.territirio(id_territirio),ruta_id varchar(20));")'''
+
+# Execute a command: this create table detalle_accidente
+'''cur.execute("create table volumetria_accidentalidad_dia.detalle_accidente(id_detalle_accidente SERIAL primary key unique,id_estado_carretera int REFERENCES volumetria_accidentalidad_dia.estado_carretera(id_estado_carretera),id_ubicacion int REFERENCES volumetria_accidentalidad_dia.ubicacion(id_ubicacion),fecha_acc int REFERENCES volumetria_accidentalidad_dia.calendar(id_calendar),dia_semana_acc varchar(20),hora_acc int);")'''
+
+
+# ------------------------------ inserted block -----------------------------------------------------
+
+ 
+'''# filling table calendar
+cur.execute("insert into volumetria_accidentalidad_dia.calendar(id_calendar, date_actual, day_name, day_of_week, month_name, year) select id_calendar, date_actual, day_name, day_of_week, month_name, extract(year from date_actual) FROM dwh.calendar")
+
+# filling table territorio
+cur.execute("insert into volumetria_accidentalidad_dia.territorio(id_territorio, territorial) select id_territorio, territorial FROM dwh.territorio")
+
+# filling table condicion_meteorologica
+cur.execute("insert into volumetria_accidentalidad_dia.condicion_meteorologica(id_cond_meteor,condic_meteor) select id_cond_meteor,condic_meteor FROM dwh.condicion_metereologica")
+
+# filling table estado_carretera
+cur.execute("insert into volumetria_accidentalidad_dia.estado_carretera(id_estado_carretera,id_cond_meteor,estado_super,terreno) select id_estado_carretera,id_cond_meteor,estado_super,terreno FROM dwh.estado_carretera")
+
+# filling table ubicacion
+cur.execute("insert into volumetria_accidentalidad_dia.ubicacion(id_ubicacion,id_territorio,ruta_id) select id_lugar_accidente,id_territorio,ruta_id FROM dwh.lugar_accidente")
+
+# filling table detalle_accidente
+cur.execute("insert into volumetria_accidentalidad_dia.detalle_accidente(id_detalle_accidente,id_estado_carretera,id_ubicacion,dia_semana_acc,hora_acc) select id_accidentes,id_estado_carretera,id_lugar_accidente,dia_semana_acc,hora_acc FROM dwh.accidentes")
+'''
+# ---------------------------- Update ---------------------------------------------------------------------
+'''
+# Update table detalle_accidente
 cur.execute(
-    "CREATE TABLE IF NOT EXISTS volumetria_accidentalidad_dia.accidentalidad_diaria (id serial PRIMARY KEY UNIQUE, idCondicionMeteor integer REFERENCES volumetria_accidentalidad_dia.condicion_meteor (id), idTerreno integer REFERENCES volumetria_accidentalidad_dia.terreno (id), idFechaAcc integer REFERENCES volumetria_accidentalidad_dia.fecha_accidente (id));")
+    """update volumetria_accidentalidad_dia.detalle_accidente as ta set fecha_acc = c.id_calendar from (select id_calendar, TB.id_accidentes from volumetria_accidentalidad_dia.calendar as TA join dwh.accidentes as TB on TA.date_actual = TB.fecha_acc) as c where c.id_accidentes = ta.id_detalle_accidente;""")
+'''
 
-
-print("TABLA CREADA CON EXITO")
-
-conn.commit()
+# test message
+print("DATOS INSERTADOS CON EXITO")
 
 # Close the cursor
 cur.close()
